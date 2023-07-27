ff400914ea6d3dc4784083c503248fbd2654fead
Sarvani <79523093+SarvaniNandipati@users.noreply.github.com>
2023-07-26 17:37:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
+# Adding all import statements on top
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+#import streamlit
 streamlit.title('My Parents New Healthy Diner')
  
 streamlit.header('Breakfast Menu')
@@ -9,7 +15,7 @@ streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
@@ -30,7 +36,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # Remove hashtag from this code incase you want to see watermelon data in json format and '#' below code
@@ -40,9 +46,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+
+#don't run anything beneath this before troubleshoot completion
+streamlit.stop()
 # Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 
-import snowflake.connector
+#import snowflake.connector
 
 #Query about Our Snowflake Trial Account Metadata 
 # changing the query to show Some Data, Instead
